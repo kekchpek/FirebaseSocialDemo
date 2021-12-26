11e0131342a30cc4bdf1c0805ef7466740cee4fe
Nikolai <nikolai.selivanov@productmadness.com>
2021-12-26 21:32:24 +0300

    Implement error popup view

--- FirebaseSocialDemoProj/Assets/Code/Views/ErrorPopup/View/ErrorPopupView.cs
+++ FirebaseSocialDemoProj/Assets/Code/Views/ErrorPopup/View/ErrorPopupView.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SocialDemo.Code.Views.ErrorPopup.View
 {
     public class ErrorPopupView : Mvp.View.View, IErrorPopupView
     {
-        public string Message { get; set; }
-        public float BackgroundTransparency { get; set; }
+        [SerializeField] private TMP_Text _messageText;
+        [SerializeField] private Image _backgroundImage;
+        [SerializeField] private Button _closeButton;
+        
+        
+        public string Message { get => _messageText.text; set => _messageText.text = value; }
+
+        public float BackgroundTransparency
+        {
+            get => 1f - _backgroundImage.color.a;
+            set =>
+                _backgroundImage.color = new Color(
+                    _backgroundImage.color.r,
+                    _backgroundImage.color.g,
+                    _backgroundImage.color.b,
+                    1f - value);
+        }
         public event Action CloseClicked;
+
+        private void Awake()
+        {
+            _closeButton.onClick.AddListener(() => CloseClicked?.Invoke());
+        }
     }
 }
