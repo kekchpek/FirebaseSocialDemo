f4610be9b28e29afbe76447f27c6dc285cb0f41b
Nikolai <nikolai.selivanov@productmadness.com>
2021-12-27 20:35:50 +0300

    Some fixes

--- FirebaseSocialDemoProj/Assets/Code/StartupBehaviour.cs
+++ FirebaseSocialDemoProj/Assets/Code/StartupBehaviour.cs
@@ -1,4 +1,6 @@
-﻿using SocialDemo.Code.Mvp.ViewManager;
+﻿using System;
+using Firebase;
+using SocialDemo.Code.Mvp.ViewManager;
 using UnityEngine;
 using Zenject;
 
@@ -10,6 +12,14 @@ namespace SocialDemo.Code
         [Inject]
         public void Initialize(IViewManager viewManager)
         {
+            FirebaseApp.Create(new AppOptions()
+            {
+                AppId = "1:918265263992:ios:2dd4bad05c038b0451ba8d",
+                ApiKey = "AIzaSyCCh0AbsxyoZ3UNV_d0FWvDBBF2nq68Q4c",
+                StorageBucket = "social-demo-firebase.appspot.com",
+                ProjectId = "social-demo-firebase",
+                DatabaseUrl = new Uri("https://social-demo-firebase-default-rtdb.europe-west1.firebasedatabase.app"),
+            });
             viewManager.OpenView(ViewDefinitions.SignIn);
         }
         
